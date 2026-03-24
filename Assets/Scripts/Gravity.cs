2c4b5ade730aaa72aa5384852e39e3813bc7c310
ZEPFINE <sasipim.saiyot@gmail.com>
2026-03-24 15:59:37 +0700

    Finish

---
+++ Assets/Scripts/Gravity.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gravity : MonoBehaviour
+{
+    Rigidbody rb;
+    const float G = 0.006674f;
+
+    public static List<Gravity> otherObjectslist;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        if (otherObjectslist == null)
+        {
+            otherObjectslist = new List<Gravity>();
+        }
+        
+        otherObjectslist.Add(this);
+    }
+
+    private void FixedUpdate()
+    {
+        foreach (Gravity obj in otherObjectslist)
+        {
+            if (obj != this)
+            {
+                Attract(obj);
+            }
+        }
+    }
+
+    void Attract(Gravity other)
+    {
+        Rigidbody otherRb = other.rb;
+        Vector3 direction = rb.position - otherRb.position;
+
+        float distance = direction.magnitude;
+
+        if(distance == 0f) { return;}
+
+        float forceMagnitude = G * (rb.mass * otherRb.mass) / Mathf.Pow(distance, 2);
+
+        Vector3 gravityForce = forceMagnitude * direction.normalized;
+        
+        otherRb.AddForce(gravityForce);
+    }
+}
